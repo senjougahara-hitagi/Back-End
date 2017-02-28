d9d124b4b69266f6c762a5cd0bb81d66dad04993
LeMinhDuc <duclm96@gmail.com>
2017-03-01 01:49:27 +0700

    smt not important

--- server/api/data/data.controller.js
+++ server/api/data/data.controller.js
@@ -1,7 +1,23 @@
 'use strict';
-var data = require('./shopData.json');
+var data = require('./shop-data');
+var fs = require('fs');
+var path = require('path');
+
 module.exports = {
   getAll : function(req, res){
+    // fs.readFile(path.join(__dirname, './shopData.json'), function(err, data){
+    //   if(err) {
+    //     console.log(err);
+    //     res.status(500).json(err);
+    //   }else {
+    //     // res.status(200).json(JSON.parse(data.toString()));
+    //     res.json(JSON.parse(data));
+    //     // console.log(JSON.parse(data));
+    //   }
+    // });
+
+
     res.json(data);
+
   }
 }
