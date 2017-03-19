fe44d034dd2ea29ef7c0dc43a6f39c6a8909b513
LeMinhDuc <duclm96@gmail.com>
2017-03-19 15:10:49 +0700

    admin manager

--- client/admin/admin.js
+++ client/admin/admin.js
@@ -1,23 +1,25 @@
 console.log("admin loaded");
 
 $( document ).ready(function() {
-  $('#searchForm').submit(function(e) {
+  var source = $("#list_member").html();
+  var listMember = Handlebars.compile(source);
+  $("#search_form").submit(function(e) {
     e.preventDefault();
-    var submitdata = $("#searchForm").serialize();
-    {
-      name: value
-      role: value
-      username: value
-    }
-    console.log("event", submitdata);
+    var data = $(this).serialize();
+    console.log("form data  ",data);
     $.ajax({
-      url: '../api/user/find',
-      data: submitdata,
-      method: ''
-    }).done(function(data){
-      console.log("data",data);
-    }).fail(function(err){
-      console.log("err",err);
+      type: "get",
+      url: "/api/user/find?" + data,
+      contentType : "application/json"
+    }).then(function(data) {
+      console.log(data);
+      var obj = {items:[]};
+      obj.items = data;
+      var itemHtml = listMember(obj);
+      $("#table_member").html(itemHtml);
+    }).fail(function(err) {
+      console.log("Error " + err);
     })
+    return false;
   })
 })
